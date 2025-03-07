c2afd220fd4695ee6bdc3e393cf15d1abd699094
dhvanit2804 <dhvanitparate123@gmail.com>
2025-03-07 17:10:54 +0530

    js

---
+++ Practice/Javascript/script.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+console.log("hello");
+console.log(5+5);
+
+
+document.write("Hello World");
+document.write("<h2>Hello World</h2>");
+
+document.getElementById("h1").innerHTML = "Hello this is javascript";
+
+document.getElementById("h2").innerText = "Hello this is javascript";
