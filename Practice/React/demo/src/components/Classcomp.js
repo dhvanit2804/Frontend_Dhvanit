42f3ff8e36c78fb3e4e284a642cea930ff5ef93b
dhvanit2804 <dhvanitparate123@gmail.com>
2025-05-16 17:30:11 +0530

    react

---
+++ Practice/React/demo/src/components/Classcomp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Classcomp extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{background: "red"}}>This is Something</h1>
+      </div>
+    )
+  }
+}
---
+++ Practice/React/demo/src/components/Functioncomp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Functioncomp() {
+  return (
+    <div>
+        <p>This is Function Tag</p>
+    </div>
+  )
+}
+
+export default Functioncomp
