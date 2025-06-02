ee69321f32873803446cac510b88688d293113cd
dhvanit2804 <dhvanitparate123@gmail.com>
2025-06-02 10:00:39 +0530

    react

--- Practice/React/react-app/src/Layout/Coman/Header.jsx
+++ Practice/React/react-app/src/Layout/Coman/Header.jsx
@@ -1,45 +1,69 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Website</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-        </li>
-       
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/about" aria-expanded="false">
-            About
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/contact"  aria-expanded="false">
-            Contact
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-expanded="false">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Website
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link active" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
 
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about" aria-expanded="false">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/contact"
+                  aria-expanded="false"
+                >
+                  Contact
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-expanded="false">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
