08c3547d20c6de4cbe666d762c261bd304fbc4e3
dhvanit2804 <dhvanitparate123@gmail.com>
2025-06-09 15:10:28 +0530

    react

--- Practice/React/Simple React App/src/components/Contact.jsx
+++ Practice/React/Simple React App/src/components/Contact.jsx
@@ -24,65 +24,67 @@ const Contact = () => {
               Get in touch with us using the form below
             </p>
           </div>
-          <div className="card shadow">
-            <div className="card-body">
-              <div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="exampleFormControlInput1"
-                    className="form-label fw-bold"
-                  >
-                    Enter your name
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={form.name}
-                    name="name"
-                    onChange={getChange}
-                    id="exampleFormControlInput1"
-                    placeholder="Enter your name"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="exampleFormControlInput3"
-                    className="form-label fw-bold"
-                  >
-                    Email Address
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={form.email}
-                    onChange={getChange}
-                    className="form-control"
-                    id="exampleFormControlInput3"
-                    placeholder="name@example.com"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="exampleFormControlTextarea2"
-                    className="form-label fw-bold"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    className="form-control"
-                    id="exampleFormControlTextarea2"
-                    name="message"
-                    onChange={getChange}
-                    rows={3}
-                    value={form.message}
-                    placeholder="Enter your message"
-                  />
-                </div>
+          <div className="row">
+            <div className="card shadow">
+              <div className="card-body">
                 <div>
-                  <input
-                    type="submit"
-                    className="btn btn-primary btn-lg w-100"
-                  />
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlInput1"
+                      className="form-label fw-bold"
+                    >
+                      Enter your name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      value={form.name}
+                      name="name"
+                      onChange={getChange}
+                      id="exampleFormControlInput1"
+                      placeholder="Enter your name"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlInput3"
+                      className="form-label fw-bold"
+                    >
+                      Email Address
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      value={form.email}
+                      onChange={getChange}
+                      className="form-control"
+                      id="exampleFormControlInput3"
+                      placeholder="name@example.com"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label
+                      htmlFor="exampleFormControlTextarea2"
+                      className="form-label fw-bold"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      className="form-control"
+                      id="exampleFormControlTextarea2"
+                      name="message"
+                      onChange={getChange}
+                      rows={3}
+                      value={form.message}
+                      placeholder="Enter your message"
+                    />
+                  </div>
+                  <div>
+                    <input
+                      type="submit"
+                      className="btn btn-primary btn-lg w-100"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
