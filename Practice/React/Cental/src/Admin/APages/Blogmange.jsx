0eee28c8d99914e5c0ecfc2516019593f48170af
dhvanit2804 <dhvanitparate123@gmail.com>
2025-06-18 17:13:12 +0530

    project

--- Practice/React/Cental/src/Admin/APages/Blogmange.jsx
+++ Practice/React/Cental/src/Admin/APages/Blogmange.jsx
@@ -16,6 +16,22 @@ const Blogmange = () => {
     setBlog(res.data);
   };
 
+  const [blogdata, setBlogData] = useState({
+    id: "",
+    title: "",
+    description: "",
+    comment: "",
+    name: "",
+    date: "",
+    img: "",
+  });
+
+  const getdata = async (id) => {
+    const res = await axios.get(`http://localhost:3000/Blog/${id}`);
+    console.log(res.data);
+    setBlogData(res.data);
+  };
+
   return (
     <div>
       <Aheader />
@@ -44,7 +60,14 @@ const Blogmange = () => {
                     <td>{data.date}</td>
                     <td>{data.comment}</td>
                     <td>
-                      <button className="btn btn-info">View</button>
+                      <button
+                        className="btn btn-info"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                        onClick={() => getdata(data.id)}
+                      >
+                        View
+                      </button>
                       <button className="btn btn-success mx-2">Edit</button>
                       <button className="btn btn-danger">Delete</button>
                     </td>
@@ -53,6 +76,72 @@ const Blogmange = () => {
               })}
           </tbody>
         </table>
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Blog Data
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-body">
+                <div className="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
+                  <div className="blog-item">
+                    <div className="blog-img">
+                      <img
+                        src={blogdata.img}
+                        className="img-fluid rounded-top w-100"
+                        alt="Image"
+                        style={{ height: "300px" }}
+                      />
+                    </div>
+                    <div className="blog-content rounded-bottom p-4">
+                      <div className="blog-date">{blogdata.date}</div>
+                      <div className="blog-comment my-3">
+                        <div className="small">
+                          <span className="fa fa-user text-primary" />
+                          <span className="ms-2">{blogdata.name}</span>
+                        </div>
+                        <div className="small">
+                          <span className="fa fa-comment-alt text-primary" />
+                          <span className="ms-2">{blogdata.comment}</span>
+                        </div>
+                      </div>
+                      <a href="#" className="h4 d-block mb-3">
+                        {blogdata.title}
+                      </a>
+                      <p className="mb-3">{blogdata.description}</p>
+                      <a href="#" className>
+                        Read More <i className="fa fa-arrow-right" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
