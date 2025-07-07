1409f449bce8c4a98bb1746e72d137e05e4c66ce
dhvanit2804 <dhvanitparate123@gmail.com>
2025-07-07 21:31:27 +0530

    project

--- Practice/React/Labsky/src/Admin/Apages/Teammanage.jsx
+++ Practice/React/Labsky/src/Admin/Apages/Teammanage.jsx
@@ -15,6 +15,25 @@ const Teammanage = () => {
     setteam(res.data);
   };
 
+  const [teamdata, setteamdata] = useState({
+    id: "",
+    name: "",
+    img: "",
+    role: "",
+  });
+
+  const getdata = async (id) => {
+    const res = await axios.get(`http://localhost:3000/team/${id}`);
+    console.log(res.data);
+    setteamdata(res.data);
+  };
+
+  const removedata = async (id) => {
+    const res = await axios.delete(`http://localhost:3000/team/${id}`);
+    console.log(res.data);
+    fetchdata();
+  };
+
   return (
     <div>
       <Aheader />
@@ -39,15 +58,79 @@ const Teammanage = () => {
                     <td>{data.name}</td>
                     <td>{data.role}</td>
                     <td>
-                      <button className="btn btn-success">View</button>
-                      <button className="btn btn-info mx-2">Edit</button>
-                      <button className="btn btn-danger">Delete</button>
+                      <button
+                        className="btn btn-success"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                        onClick={() => getdata(data.id)}
+                      >
+                        View
+                      </button>
+
+                      <button
+                        className="btn btn-danger ms-2"
+                        onClick={() => removedata(data.id)}
+                      >
+                        Delete
+                      </button>
                     </td>
                   </tr>
                 );
               })}
           </tbody>
         </table>
+      </div>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Team Member
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <div className="card shadow border-0 h-100">
+                <div className="position-relative overflow-hidden">
+                  <img
+                    src={teamdata.img}
+                    className="card-img-top img-fluid"
+                    style={{ height: "500px" }}
+                  />
+                  <div className="position-absolute top-0 end-0 p-2 d-flex flex-column gap-2">
+                    <a className="btn btn-sm btn-light" href="#">
+                      <i className="fab fa-facebook-f" />
+                    </a>
+                    <a className="btn btn-sm btn-light" href="#">
+                      <i className="fab fa-twitter" />
+                    </a>
+                    <a className="btn btn-sm btn-light" href="#">
+                      <i className="fab fa-linkedin-in" />
+                    </a>
+                    <a className="btn btn-sm btn-light" href="#">
+                      <i className="fab fa-youtube" />
+                    </a>
+                  </div>
+                </div>
+                <div className="card-body text-center">
+                  <h5 className="card-title mb-1">{teamdata.name}</h5>
+                  <p className="card-text text-muted">{teamdata.role}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
