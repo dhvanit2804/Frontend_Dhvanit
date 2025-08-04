7668c749db5cd73e89f891532d661d09e9cffd18
dhvanit2804 <dhvanitparate123@gmail.com>
2025-08-04 14:22:00 +0530

    project

--- Practice/React/passop/src/components/Manager.jsx
+++ Practice/React/passop/src/components/Manager.jsx
@@ -1,36 +1,101 @@
 import React, { useState, useEffect } from "react";
+import { useRef } from "react";
 
 const Manager = () => {
+  const ref = useRef();
+  const [form, setform] = useState({
+    site: "",
+    username: "",
+    password: "",
+  });
+  const [passwordArray, setpasswordArray] = useState([]);
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if (passwords) {
+      setpasswordArray(JSON.parse(passwords));
+    }
+  }, []);
+
+  const showPassword = () => {
+    alert("Show the Password");
+    if (ref.current.src.includes("icons/eyecross.png")) {
+      ref.current.src = "icons/eye.png";
+    } else {
+      ref.current.src = "icons/eyecross.png";
+    }
+  };
+
+  const savePassword = () => {
+    console.log(form);
+  };
+
+  const handleChange = (e) => {
+    setform({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <>
-      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-green-400 opacity-20 blur-[100px]"></div>
       </div>
-      <div className="bg-slate-50 mycontainer">
+      <div className="mycontainer">
         <h1 className="text-4xl font-bold text-center">
-          <span className="text-green-700">/ &lt;</span>
+          <span className="text-green-500">/ &lt;</span>
           <span>Pass</span>
-          <span className="text-green-700">OP/ &gt;</span>
+          <span className="text-green-500">OP/ &gt;</span>
         </h1>
         <p className="text-green-900 text-lg text-center">
           Your Own Password Manager
         </p>
         <div className="text-black flex flex-col p-4 gap-8 items-center">
           <input
+            onChange={handleChange}
+            placeholder="Enter Website URL"
+            value={form.site}
+            name="site"
             className="rounded-full bg-white border border-green-500 w-full p-4 py-1"
             type="text"
           />
           <div className="flex w-full justify-between gap-8">
             <input
+              onChange={handleChange}
+              placeholder="Enter Username"
+              value={form.username}
+              name="username"
               className="rounded-full bg-white border border-green-500 w-full p-4 py-1"
               type="text"
             />
-            <input
-              className="rounded-full bg-white border border-green-500 w-full p-4 py-1"
-              type="text"
-            />
+            <div className="relative">
+              <input
+                onChange={handleChange}
+                placeholder="Enter Password"
+                value={form.password}
+                name="password"
+                className="rounded-full bg-white border border-green-500 w-full p-4 py-1"
+                type="text"
+              />
+              <span
+                className="absolute right-[3px] top-[4px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1"
+                  width={26}
+                  src="icons/eye.png"
+                  alt=""
+                />
+              </span>
+            </div>
           </div>
-          <button className="flex justify-center items-center bg-green-600 hover:bg-green-500 rounded-full px-2 py-2 w-fit">
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 border cursor-pointer border-green-900 py-2 w-fit"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
               trigger="hover"
--- Practice/React/passop/src/components/Navbar.jsx
+++ Practice/React/passop/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     <nav className="bg-slate-800 text-white">
       <div className="mycontainer flex justify-between items-center px-4 h-14 py-5">
         <div className="logo font-bold text-2xl">
-          <span className="text-green-700">/ &lt;</span>
+          <span className="text-green-500">/ &lt;</span>
           <span>Pass</span>
-          <span className="text-green-700">OP/ &gt;</span>
+          <span className="text-green-500">OP/ &gt;</span>
         </div>
         <ul>
           <li className="flex gap-4">
