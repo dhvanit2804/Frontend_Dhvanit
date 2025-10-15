fd6450d624bc8001398c93b08418f1ed3f344591
dhvanit2804 <dhvanitparate123@gmail.com>
2025-10-15 12:58:07 +0530

    15-10-2025

--- PROJECT/Chatbot/backend/index.js
+++ PROJECT/Chatbot/backend/index.js
@@ -1,11 +1,24 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
 const app = express()
-const port = 3000
+dotenv.config()
+
+const port = process.env.PORT || 3000
+
+// Database Connection Code
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+  console.log("Connected to MongoDB")
+}).catch((error)=>{
+  console.log("Error Connecting to MongoDB:", error)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello My Name is Dhvanit!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is Running on Port ${port}`)
 })
